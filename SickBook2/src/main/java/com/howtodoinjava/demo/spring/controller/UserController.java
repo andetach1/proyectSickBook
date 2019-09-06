df8edc0b9b2c11d3a4632f124b3d9d8028a177db
andetach1 <catachack80@ucatolica.edu.co>
2019-09-05 21:39:52 -0500

    Update UserController.java

--- SickBook2/src/main/java/com/howtodoinjava/demo/spring/controller/UserController.java
+++ SickBook2/src/main/java/com/howtodoinjava/demo/spring/controller/UserController.java
@@ -18,29 +18,75 @@ import com.howtodoinjava.demo.spring.service.UserService;
 @Controller
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@GetMapping("/editUser") //llama al home
-	public String userForm(Locale locale, Model model) {
-		model.addAttribute("users", userService.list());
-		return "editUsers";
-	}
-	
-	@ModelAttribute("user")
+    @GetMapping("/editUser") //llama al home
+    public String userForm(Locale locale, Model model) {
+        model.addAttribute("users", userService.list());
+        return "editUsers";
+    }
+    
+    @GetMapping("/registro") //llama al registro
+    public String registroForm(Locale locale) {
+        return "registro";
+    }
+    @GetMapping("/login") //llama al login
+    public String loginForm(Locale locale) {
+        return "login";
+    }
+    @GetMapping("/perfil") //llama al perfil
+    public String perfilForm(Locale locale) {
+        return "perfilprincipal";
+    }
+    @GetMapping("/consultas") //llama al consultas
+    public String consultasForm(Locale locale) {
+        return "consultas";
+    }
+    @GetMapping("/recomendaciones") //llama al consultas
+    public String recomendacionesForm(Locale locale) {
+        return "recomendaciones";
+        
+    }
+    @GetMapping("/preguntas") //llama al consultas
+    public String preguntasForm(Locale locale) {
+        return "formulariopreguntas";
+    }
+    
+    @GetMapping("/sintomaCabeza") //llama al consultas
+    public String sintomaCabezaForm(Locale locale) {
+        return "sintomaCabeza";
+    }
+    
+    @GetMapping("/sintomaEstomago") //llama al consultas
+    public String sintomaEstomagoForm(Locale locale) {
+        return "sintomaEstomago";
+    }
+    
+    @GetMapping("/sintomaArticulaciones") //llama al consultas
+    public String sintomaArticulacionesForm(Locale locale) {
+        return "sintomaArticulaciones";
+    }
+    
+    @GetMapping("/sintomaPecho") //llama al consultas
+    public String sintomaPechoForm(Locale locale) {
+        return "sintomaPecho";
+    }
+    
+    @ModelAttribute("user")
     public User formBackingObject() {
         return new User();
     }
 
-	@PostMapping("/addUser")
-	public String saveUser(@ModelAttribute("user") @Valid User user, BindingResult result, Model model) {
+    @PostMapping("/addUser")
+    public String saveUser(@ModelAttribute("user") @Valid User user, BindingResult result, Model model) {
 
-		if (result.hasErrors()) {
-			model.addAttribute("users", userService.list());
-			return "editUsers";
-		}
+        if (result.hasErrors()) {
+            model.addAttribute("users", userService.list());
+            return "login";
+        }
 
-		userService.save(user);
-		return "redirect:/editUser";//matener en el usuarios
-	}
+        userService.save(user);
+        return "redirect:/login";//matener en el usuarios
+    }
 }
